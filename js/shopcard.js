9a950f288ebbb89c4df2d8f6478c786a1463b5c9
lalahummatova <lalath@code.edu.az>
2020-05-03 23:50:31 +0400

    Filter is done

--- js/shopcard.js
+++ js/shopcard.js
@@ -1,3 +1,59 @@
+(function(){
+
+  'use strict';
+
+
+  var $projects = $('.projects');
+
+  $projects.isotope({
+      itemSelector: '.item',
+      layoutMode: 'fitRows'
+  });
+
+  $(".elements .btn").on('click', function(e){
+
+      e.preventDefault();
+
+      var filter = $(this).attr('data-filter');
+
+      $(".elements .btn").removeClass('active');
+      $(this).addClass('active');
+
+      $projects.isotope({filter: filter});
+
+  });
+
+  $('.card').mouseenter(function(){
+
+      $(this).find('.card-overlay').css({'top': '-100%'});
+      $(this).find('.card-hover').css({'top':'0'});
+
+  }).mouseleave(function(){
+
+      $(this).find('.card-overlay').css({'top': '0'});
+      $(this).find('.card-hover').css({'top':'100%'});
+
+  });
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   $(document).ready(function(){
 
@@ -28,28 +84,3 @@
   })
 
 
-$(document).ready(  (function(){
-
-    'use strict';
-
-    var $projects = $('.projects');
-
-    $projects.isotope({
-        itemSelector: '.item',
-        layoutMode: 'fitRows'
-    });
-
-    $(".elements .btn").on('click', function(e){
-
-        e.preventDefault();
-
-        var filter = $(this).attr('data-filter');
-
-        $(".elements .btn").removeClass('active');
-        $(this).addClass('active');
-
-        $projects.isotope({filter: filter});
-
-    });
-
-}))
